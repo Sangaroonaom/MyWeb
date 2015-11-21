e23dd8a54cdfe6d1f183d45c43c5bcbea18c4fba
Sangaroonaom <aom_sangaroon@hotmail.com>
2015-11-22 00:46:19 +0700

    Update index.php

--- index.php
+++ index.php
@@ -8,6 +8,12 @@
 <center><a href="http://image.goosiam.com/view.asp?uid=115979&s=KLRQ3D4yLUAG" target="_blank"><img border="0" src="http://image.goosiam.com/imgupload/l/KLRQ3D4yLUAG.jpg"/></a>
 
 </center>
-
+<br>
+<H3 align=center> My Profile </H3>
+<center> My name is : Sangaroon Duangnin<br>
+         StudentID : 5605104047<br>
+         Age : 21   Gender :Female<br>
+         My major is Information communication Technology.
+         
 </body>
 </html>
